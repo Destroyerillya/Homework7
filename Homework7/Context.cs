ce40c8107da7ec37824ecc3aeab528b9dc9e724d
Destroyer <kasatkinilyaandreevich@gmail.com>
2021-03-27 02:07:38 +0300

    Initial commit

---
+++ Homework7/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    public class Context: DbContext
+    {
+        public DbSet<UserData> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=blog;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Homework7/UserData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blog
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        
+        public string Country { get; set;}
+        
+        public string City { get; set; }
+    }
+}
